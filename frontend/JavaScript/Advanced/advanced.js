27b797afa0dbd4529fe845a2900ae5fe401c441b
manuelabordad <manuelaborda2019@gmail.com>
2023-04-26 10:05:35 -0500

    queriesss

--- frontend/JavaScript/Advanced/advanced.js
+++ frontend/JavaScript/Advanced/advanced.js
@@ -1,3 +1,5 @@
+//QUERYS ----------------------------------------------------
+
 let linkElement = document.getElementById("extrernal-link"); //return the element
 linkElement.href = "https://google.com"; //change the href
 
@@ -15,3 +17,25 @@ firstElement.parentElement.removeChild(firstElement); //OLD BROWSERS ONLY
 
 //Move HTML element
 firstElement.parentElement.append(firstParagraph);
+
+//inner HTML
+firstParagraph.innerHTML = "Hi this is <strong>important !</strong>"; //access to the html content in the element
+
+//EVENTS ------------------------------------------------------------
+
+let paragraphElement = document.querySelector("p");
+function changeParagraphText() {
+	paragraphElement.textContent = "Clicked!";
+}
+paragraphElement.addEventListener("click", changeParagraphText); // parameters:  type , fucntion to execute
+//also known as callbackfuntion -> fun that is passed as parameter to another function
+
+let inputElement = document.querySelector("input");
+function retriveUserInput(event) {
+	// event automatic generated parameter
+	// let enteredText = inputElement.value; //holds the value in that input
+	//Alternative
+	let enteredText = event.target.value; //get rid of the external variable inputE
+	console.log(enteredText);
+}
+inputElement.addEventListener("input");
